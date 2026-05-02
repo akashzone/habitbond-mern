79e38f7c8a1ef23bc8e4534838a8bacc946fa2af
akashzone <akashnadar.dev@gmail.com>
2026-05-02 10:30:15 +0530

    build a helper function to make api calls

---
+++ frontend/src/service/api.js
@@ -0,0 +1,23 @@
+const BASE_URL = "http://localhost:5000/api";
+
+// helper function
+export const apiFetch = async (url, options = {}) => {
+  const token = localStorage.getItem("token");
+
+  const res = await fetch(`${BASE_URL}${url}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(token && { Authorization: `Bearer ${token}` }),
+      ...(options.headers || {}),
+    },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+
+  return data;
+};
